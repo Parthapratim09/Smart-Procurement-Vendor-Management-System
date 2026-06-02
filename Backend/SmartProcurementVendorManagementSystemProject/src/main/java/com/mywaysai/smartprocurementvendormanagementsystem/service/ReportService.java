7311395ead3eeac9f06f933dc5eac644c4fbb339
Parthapratim09 <parthapratimmakhal09@gmail.com>
2026-06-02 10:30:58 +0530

    First commit

---
+++ Backend/SmartProcurementVendorManagementSystemProject/src/main/java/com/mywaysai/smartprocurementvendormanagementsystem/service/ReportService.java
@@ -0,0 +1,20 @@
+package com.mywaysai.smartprocurementvendormanagementsystem.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.mywaysai.smartprocurementvendormanagementsystem.entity.PurchaseOrder;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ReportService {
+
+    private final PurchaseOrderRepository repository;
+
+    public List<PurchaseOrder> spendAnalysis(){
+        return repository.findAll();
+    }
+}
